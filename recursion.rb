069a3e0a272cd35bfd002468759a895645732b45
Jet <jetpoke1011@gmail.com>
2023-05-17 12:51:01 -0400

    create fib adn bsearch

--- recursion.rb
+++ recursion.rb
@@ -43,32 +43,83 @@ end
 def deep_dup(arr)
   dup_arr = []
   return [] unless arr.is_a?(Array)
-
+  
   arr.each do |ele|
     if ele.is_a?(Array)
-        deep_dup(ele)
-    else
-       dup_arr += [ele]
+        dup_arr << deep_dup(ele)
+    else 
+        dup_arr << ele.dup
     end
-  end
+
+end
+  dup_arr
 
 end
 
 
-robot_parts = [
-  ["nuts", "bolts", "washers"],
-  ["capacitors", "resistors", "inductors"]
-]
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
 
- dup = deep_dup(robot_parts)
+#  dup = deep_dup(robot_parts)
 
- dup[0] << "LEDs"
+#  dup[0] << "LEDs"
 
- p dup
- p robot_parts
+#  p dup
+#  p robot_parts
+
+def itr_fibonacci(n)
+
+    sequence = [0,1,1]
+    sequence.take(n )if n <= 3
+    (2..n).each do |i|
+        sequence << (sequence[i] + sequence[i-1])
+    end
+    return sequence
+
+end 
+
+def rec_fibonacci(n)
+    
+    return [0,1,1].take(n) if n <= 3
+
+    n_fib = rec_fibonacci(n-1)
+    n_fib << n_fib[-2] + n_fib[-1]
+end 
+
+def bsearch(arr,target)
+    sorted = arr.sort 
+    mid = arr.length /2 
+
+    if mid == target
+        return sorted.index(mid)
+    end 
+
+    if target < mid
+        return bsearch(sorted([0..mid]))
 
 
 
+   
+
+
+
+end 
+
+def merge_sort(arr)
+end 
+
+def subsets(arr)
+end 
+
+def permutations(arr)
+end 
+
+
+
+    
+    
 
 
 
